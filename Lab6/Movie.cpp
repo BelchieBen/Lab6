3dd47105778c0d7fa9ef6ccc6b7bc43275d233a0
benjamin.belcher <benjamin.belcher@ideagen.com>
2022-01-18 16:33:09 +0000

    Added cpp file for movie

---
+++ Lab6/Movie.cpp
@@ -0,0 +1,53 @@
+#include "Movie.h"
+
+Movie::Movie(
+	string film,
+	string genre,
+	string studio,
+	int audienceScore,
+	float profitability,
+	int rottenTomatoesScore,
+	double worldwideGross,
+	string year){
+
+	this->film = film;
+	this->genre = genre;
+	this->studio = studio;
+	this->audienceScore = audienceScore;
+	this->profitability = profitability;
+	this->rottenTomatoesScore = rottenTomatoesScore;
+	this->worldwideGross = worldwideGross;
+	this->year = year;
+}
+
+string Movie::getFilm() {
+
+}
+
+string Movie::getGenre() {
+
+}
+
+string Movie::getStudio() {
+
+}
+
+int Movie::getAudienceScore() {
+
+}
+
+float Movie::getProfitability() {
+
+}
+
+int Movie::getRottenTomatoesScore() {
+
+}
+
+double Movie::getWorldwideGross() {
+
+}
+
+string Movie::getYear() {
+
+}
